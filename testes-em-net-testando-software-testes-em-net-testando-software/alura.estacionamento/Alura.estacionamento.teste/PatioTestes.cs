05f28e4eca7dcdba24b1d0ffe15f287cad7fb591
Juliolimahen <juliolima_henrique@outlook.com>
2022-08-10 12:06:12 -0300

    Testes de software

---
+++ testes-em-net-testando-software-testes-em-net-testando-software/alura.estacionamento/Alura.estacionamento.teste/PatioTestes.cs
@@ -0,0 +1,60 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.estacionamento.teste
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            var estacionamento = new Patio();
+
+            veiculo.Proprietario = "André Silva";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "asd-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert 
+            Assert.Equal(2, faturamento);
+        }
+        [Theory]
+        [InlineData("André Silva", "ASD-1498", "preto", "Gol")]
+        [InlineData("José Silva", "ASD-1497", "prata", "Uno")]
+        [InlineData("Anderson Silva", "ASD-1499", "braco", "Celta")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Placa = placa;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
---
+++ testes-em-net-testando-software-testes-em-net-testando-software/alura.estacionamento/Alura.estacionamento.teste/VeiculoTestes.cs
@@ -0,0 +1,34 @@
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.estacionamento.teste
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestarVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Acelerar(10);
+
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Frear(10);
+
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
