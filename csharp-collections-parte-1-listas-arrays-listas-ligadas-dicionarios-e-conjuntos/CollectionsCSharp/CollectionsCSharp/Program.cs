bdefd45c2a183037a8c08963f315041f4b13ffc9
Juliolimahen <juliolima_henrique@outlook.com>
2023-03-22 15:11:56 -0300

    tranbalando com arrays e listas

---
+++ csharp-collections-parte-1-listas-arrays-listas-ligadas-dicionarios-e-conjuntos/CollectionsCSharp/CollectionsCSharp/Program.cs
@@ -0,0 +1,108 @@
+﻿
+string aulaIntro = "Introdução às Coleções";
+string aulaModelando = "Modelando a Classe Aula";
+string aulaSets = "Trabalhando com Conjuntos";
+
+// Primeira forma
+string[] aulas = new string[]
+{
+    aulaIntro,
+    aulaModelando,
+    aulaSets
+};
+
+// Segunda Forma
+
+string[] aulas1 = new string[3];
+
+aulas1[0] = aulaIntro;
+aulas1[1] = aulaModelando;
+aulas1[2] = aulaSets;
+
+//Pecorrendo array com for
+//PercorrendoArrayComFor(aulas1);
+
+//Percorrendo array com forech
+//PercorrendoArrayComForeach(aulas);
+
+//IndexOf
+Console.WriteLine("Aulas modelando está no indice " + Array.IndexOf(aulas1, aulaModelando));
+Console.WriteLine();
+Console.WriteLine(Array.LastIndexOf(aulas1, aulaModelando));
+Console.WriteLine();
+//Método reverse, reverte a ordem do array
+Array.Reverse(aulas1);
+
+PercorrendoArrayComFor(aulas1);
+
+Console.WriteLine();
+
+//Voltar para ordem original
+Array.Reverse(aulas1);
+
+PercorrendoArrayComFor(aulas1);
+
+Console.WriteLine();
+
+Array.Resize(ref aulas1, 2);
+
+PercorrendoArrayComFor(aulas1);
+
+Console.WriteLine();
+
+Array.Resize(ref aulas1, 3);
+
+PercorrendoArrayComFor(aulas1);
+
+Console.WriteLine();
+
+//Acessar ultima posição do array
+aulas1[aulas1.Length - 1] = "conclusão";
+
+PercorrendoArrayComFor(aulas1);
+
+Console.WriteLine();
+
+//Ordernar o Arry
+Array.Sort(aulas1);
+
+string[] copia = new string[2];
+
+//Copiar as duas ultimas aulas do array
+Array.Copy(aulas1, 1, copia, 0, 2);
+
+PercorrendoArrayComFor(aulas1);
+
+Console.WriteLine();
+
+// Clonar array
+// Cast
+string[] clone = aulas1.Clone() as string[];
+
+PercorrendoArrayComFor(aulas1);
+
+Console.WriteLine();
+
+Array.Clear(clone, 1, 2);
+
+PercorrendoArrayComFor(clone);
+
+Console.WriteLine();
+
+//Pecorrendo array com for
+static void PercorrendoArrayComFor(string[] aulas1)
+{
+    for (int i = 0; i < aulas1.Length; i++)
+    {
+        Console.WriteLine(aulas1[i]);
+    }
+}
+
+//Percorrendo array com forech
+static void PercorrendoArrayComForeach(string[] aulas)
+{
+    foreach (string aulas2 in aulas)
+    {
+        Console.WriteLine(aulas2);
+    }
+}
---
+++ csharp-collections-parte-1-listas-arrays-listas-ligadas-dicionarios-e-conjuntos/CollectionsCSharp/CollectionsCSharpList/Program.cs
@@ -0,0 +1,122 @@
+﻿
+string aulaIntro = "Introdução às Coleções";
+string aulaModelando = "Modelando a Classe Aula";
+string aulaSets = "Trabalhando com Conjuntos";
+
+//Uma lista é um array dinamico
+//Ela aumenta e diminui de acordo com a necessidade
+List<string> aulas = new List<string>();
+//{
+//    aulaIntro,
+//    aulaModelando,
+//    aulaSets
+//};
+
+aulas.Add(aulaIntro);
+aulas.Add(aulaModelando);
+aulas.Add(aulaSets);
+
+Console.WriteLine("Imprimindo com método anônimo");
+
+//Imprimindo com método anônimo
+aulas.ForEach(aula =>
+{
+    Console.WriteLine(aula);
+});
+
+Console.WriteLine("\nImprimindo com laço For");
+
+PercorrendoListaComFor(aulas);
+
+Console.WriteLine("\nImprimindo com laço Foreach");
+
+PercorrerListaComForeach(aulas);
+
+Console.WriteLine("\nAcessando aula pelo indice");
+
+Console.WriteLine("A primeira aula é " + aulas[0]);
+
+Console.WriteLine("A última aula é " + aulas[aulas.Count - 1]);
+
+Console.WriteLine("\nAcessando aula pelo método First");
+
+Console.WriteLine("A primeira aula é " + aulas.FirstOrDefault());
+
+Console.WriteLine("\n Acessando aula pelo método Last");
+
+Console.WriteLine("A última aula é " + aulas.LastOrDefault());
+
+//Adicionado aula pelo indice
+aulas[0] = "Trabalhando com listas";
+
+PercorrerListaComForeach(aulas);
+
+Console.WriteLine("\nOperações com listas ");
+
+//usar OrDefault para não gerar exceção
+Console.WriteLine("A primeira aula 'Trabalhando' é: " +
+    aulas.FirstOrDefault(aula => aula.Contains("Trabalhando")));
+
+Console.WriteLine("A ultima aula 'Trabalhando' é: " +
+    aulas.LastOrDefault(aula => aula.Contains("Trabalhando")));
+
+
+Console.WriteLine("\nRevertendo a ordem da lista");
+
+aulas.Reverse();
+
+PercorrerListaComForeach(aulas);
+
+Console.WriteLine("\nRemovando ultimo item");
+
+aulas.RemoveAt(aulas.Count - 1);
+
+PercorrerListaComForeach(aulas);
+
+Console.WriteLine("\nAdicionado a aula 'conclusão'");
+
+aulas.Add("Conclusão");
+
+PercorrerListaComForeach(aulas);
+
+Console.WriteLine("\nOrdenando lista");
+
+aulas.Sort();
+
+PercorrerListaComForeach(aulas);
+
+Console.WriteLine("\nFazendo copia de uma lista ");
+
+List<string> copia = aulas.GetRange(aulas.Count - 2, 2);
+
+PercorrerListaComForeach(copia);
+
+Console.WriteLine("\nClonando lista");
+
+List<string> clone = new List<string>(aulas);
+
+PercorrerListaComForeach(clone);
+
+Console.WriteLine("\nRemovendo dois ultimos itens");
+
+clone.RemoveRange(aulas.Count - 2, 2);
+
+Console.WriteLine();
+
+PercorrerListaComForeach(clone);
+
+static void PercorrerListaComForeach(List<string> aulas)
+{
+    foreach (string aula in aulas)
+    {
+        Console.WriteLine(aula);
+    }
+}
+
+static void PercorrendoListaComFor(List<string> aulas)
+{
+    for (int i = 0; i < aulas.Count; i++)
+    {
+        Console.WriteLine(aulas[i]);
+    }
+}
