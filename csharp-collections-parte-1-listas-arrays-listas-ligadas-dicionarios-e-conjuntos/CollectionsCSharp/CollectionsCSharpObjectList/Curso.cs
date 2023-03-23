7dbdd31114af27e449abf9a666c5ff957575454d
Juliolimahen <juliolima_henrique@outlook.com>
2023-03-22 21:54:34 -0300

    CollectionsCSharpSets

---
+++ csharp-collections-parte-1-listas-arrays-listas-ligadas-dicionarios-e-conjuntos/CollectionsCSharp/CollectionsCSharpObjectList/Curso.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsCSharpObjectList;
+
+class Curso
+{
+    private List<Aula> aulas;
+    private string nome;
+    private string instrutor;
+
+    public Curso(string nome, string instrutor)
+    {
+        this.nome = nome;
+        this.instrutor = instrutor;
+        this.aulas = new List<Aula>();
+    }
+
+    public string Nome
+    {
+        get { return nome; }
+        set { nome = value; }
+    }
+
+    //Protegendo lista contra acessos externos
+    public IList<Aula> Aulas
+    {
+        get { return new ReadOnlyCollection<Aula>(aulas); }
+    }
+
+    public string Instrutor
+    {
+        get { return instrutor; }
+        set { instrutor = value; }
+    }
+
+    public void Adiciona(Aula aula)
+    {
+        this.aulas.Add(aula);
+    }
+
+    public int TempoTotal
+    {
+        get
+        {
+            //Totalizador logica basica
+
+            //int total = 0;
+            //foreach (Aula aula in aulas)
+            //{
+            //    total += aula.Tempo;
+            //}
+            //return total;
+
+            //Totalizador usando linq
+            return aulas.Sum(aula => aula.Tempo);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Curso: {nome}, Tempo: {TempoTotal}, Aulas:{string.Join(", ",aulas)}";
+    }
+}
--- csharp-collections-parte-1-listas-arrays-listas-ligadas-dicionarios-e-conjuntos/CollectionsCSharp/CollectionsCSharpObjectList/Program.cs
+++ csharp-collections-parte-1-listas-arrays-listas-ligadas-dicionarios-e-conjuntos/CollectionsCSharp/CollectionsCSharpObjectList/Program.cs
@@ -5,6 +5,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        Curso curso = new Curso("C# Collections", "Marcelo Oliveira");
+
+        ////Code smell, exposição indecente
+        //curso.Aulas.Add(new Aula("Trabalhando com listas", 21));
+
+        curso.Adiciona(new Aula("Trabalhando com listas", 21));
+        Imprimir(curso.Aulas);
+
+        curso.Adiciona(new Aula("Criando uma Aula", 20));
+        curso.Adiciona(new Aula("Modelando com Coleções", 19));
+
+        Imprimir(curso.Aulas);
+
+        //Copiar a lista para outra lista 
+        List<Aula> aulasCopia = new List<Aula>(curso.Aulas);
+
+        // Ordernar a copia
+        aulasCopia.Sort();
+        Imprimir(aulasCopia);
+
+        //Totalizar o tempo do curso
+        Console.WriteLine(curso.TempoTotal);
+
+        Console.WriteLine(curso);
+    }
+
+    private static void OperacoesComListasDeObjetos()
+    {
         var aulaIntro = new Aula("Introdução às Coleções", 20);
         var aulaModelando = new Aula("Modelando a Classe Aula", 18);
         var aulaSets = new Aula("Trabalhando com Conjuntos", 16);
@@ -23,42 +51,16 @@ class Program
 
         //Lambda para comparar
         aulas.Sort((x, y) => x.Tempo.CompareTo(y.Tempo));
-        
+
         Imprimir(aulas);
     }
 
-    private static void Imprimir(List<Aula> aulas)
+    private static void Imprimir(IList<Aula> aulas)
     {
         Console.Clear();
         foreach (Aula aula in aulas)
         {
             Console.WriteLine(aula);
         }
-    }
-}
-
-class Aula : IComparable
-{
-    private string titulo;
-    private int tempo;
-
-    public Aula(string titulo, int tempo)
-    {
-        this.titulo = titulo;
-        this.tempo = tempo;
-    }
-
-    public string Titulo { get => titulo; set => titulo = value; }
-    public int Tempo { get => tempo; set => tempo = value; }
-
-    public int CompareTo(object? obj)
-    {
-        Aula that = obj as Aula;
-        return this.titulo.CompareTo(that.titulo);
-    }
-
-    public override string ToString()
-    {
-        return $"[título: {Titulo}, tempo: {Tempo} minutos]";
     }
 }
