412577577b16a47fed5eada117276498e4fde661
Juliolimahen <juliolima_henrique@outlook.com>
2023-03-24 23:57:38 -0300

    Consulta Linq

---
+++ csharp-collections-parte-2-colecoes-e-linq/CollectionsCSharpSortedList/CollectionsCSharpConsultas/Mes.cs
@@ -0,0 +1,24 @@
+﻿namespace CollectionsCSharpConsultas
+{
+    internal class Mes //: IComparable<Mes>
+    {
+        public Mes(string nome, int dias)
+        {
+            Nome = nome;
+            Dias = dias;
+        }
+
+        public string Nome { get; private set; }
+        public int Dias { get; private set; }
+
+        //public int CompareTo(Mes? other)
+        //{
+        //    return this.Nome.CompareTo(other.Nome);
+        //}
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Dias}";
+        }
+    }
+}
--- csharp-collections-parte-2-colecoes-e-linq/CollectionsCSharpSortedList/CollectionsCSharpConsultas/Program.cs
+++ csharp-collections-parte-2-colecoes-e-linq/CollectionsCSharpSortedList/CollectionsCSharpConsultas/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CollectionsCSharpConsultas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mes> meses = new List<Mes>
+            {
+                new Mes("Janeiro", 30),
+                new Mes("Janeiro", 31),
+                new Mes("Fevereiro", 28),
+                new Mes("Março", 31),
+                new Mes("Abril", 30),
+                new Mes("Maio", 31),
+                new Mes("Junho", 30),
+                new Mes("Julho", 31),
+                new Mes("Agosto", 31),
+                new Mes("Setembro", 30),
+                new Mes("Outubro", 31),
+                new Mes("Novembro", 30),
+                new Mes("Dezembro", 31)
+            };
+
+            //meses.Sort();
+
+            //foreach (var mes in meses)
+            //{
+            //    if (mes.Dias == 31)
+            //    {
+            //        Console.WriteLine(mes.Nome.ToUpper());
+            //    }
+            //}
+
+            //LINQ = Consulta integrada à linguagem 
+
+            IEnumerable<string>
+                consulta = meses
+                            .Where(m => m.Dias == 31)
+                            .OrderBy(m => m.Nome)
+                            .Select(m => m.Nome.ToUpper());
+
+            Imprimir(consulta);
+        }
+
+        private static void Imprimir(IEnumerable<string> consulta)
+        {
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
